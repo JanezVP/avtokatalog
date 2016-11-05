f21e15340b1679c3e790da7c46c04960ff543b47
JanezVP <janez.voler@gmail.com>
2016-11-05 11:42:41 +0100

    pogled po kilometrih in po servisu

--- avtokatalog.py
+++ avtokatalog.py
@@ -10,6 +10,7 @@ class vozni_park (object):
         return (self.znamka  + ", " + self.model  + ", " "prevozenih: " + self.stevilo_kilometrov  + " km" + ", " + "zadnji servis: " + self.servis)
 
 
+
 prvi = vozni_park(znamka ="fiat", model="punto", stevilo_kilometrov="10", servis="1980")
 drugi = vozni_park(znamka ="opel", model="astra", stevilo_kilometrov="100", servis="2000")
 
@@ -21,8 +22,22 @@ for tretji in avtokatalog:
     print  tretji.izpisi_katalog()
 
 while True:
-    odgovor = raw_input("Zelite koncati? da/ne ")
+    odgovor = raw_input("Zelite stevilo prevozenih kilometrov za vsak avto posebej? da/ne ")
     if odgovor == "da":
+        for i in avtokatalog:
+            print i.znamka + ", "  + i.model + " Prevozenih kilometrov: "+ i.stevilo_kilometrov + " km"
+    break
+
+while True:
+    odgovor = raw_input("Zelite zadnji servis za vsak avto posebej? da/ne ")
+    if odgovor == "da":
+        for i in avtokatalog:
+            print i.znamka + ", "  + i.model + " Servis: "+ i.servis + " leta"
+    break
+
+while True:
+    odgovor = raw_input("Zelite dodati novo vozilo? da/ne ")
+    if odgovor != "da":
                         break
     znamka = raw_input("znamka ")
     model = raw_input("model ")
